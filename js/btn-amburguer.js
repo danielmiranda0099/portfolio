897bd514fd48dc69bc666a131a017647b4c896e2
dmc0099 <danielmesa877@gmail.com>
2021-04-12 22:03:27 -0500

    fixes in the mobile menu and font details

--- js/btn-amburguer.js
+++ js/btn-amburguer.js
@@ -4,10 +4,16 @@
 export function btnAmburguer(){
     const $amburguer = document.getElementById("id-btn-amburguer");
     const $header = document.querySelector('header');
+
     document.addEventListener("click", (e) =>{
-        if(e.target.matches("#id-btn-amburguer")){
+        if(e.target.matches("#id-btn-amburguer") || e.target.matches("#id-btn-amburguer *")){
             $amburguer.classList.toggle("open");
             $header.classList.toggle("headerOpen");
+        }
+
+        if(e.target.matches(".enlaces")){
+            $amburguer.classList.remove("open");
+            $header.classList.remove("headerOpen");
         }
     });
     
