bf3a1a121f5e3c7c9ec76a92a5b25bfc2c6c82bb
dmc0099 <danielmesa877@gmail.com>
2021-10-07 12:45:00 -0500

    small modifications were added

--- js/preloaderPage.js
+++ js/preloaderPage.js
@@ -3,5 +3,5 @@ const d = document;
 export function preloaderPage() {
     setTimeout(() => {
         d.querySelector('.is-total-modal').classList.add('is-invisible');
-    }, 1600);
+    }, 1200);
 }
